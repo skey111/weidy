81037b214130e709fb5e924df6b20fd4c6176fab
skey111 <skey111@foxmail.com>
2020-01-02 06:12:54 +0800

    add jiaocheng

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,8 @@
 cnzz_s_tag.type = "text/javascript";
 cnzz_s_tag.async = true;
 cnzz_s_tag.charset = "utf-8";
-cnzz_s_tag.src = "http://w.cnzz.com/c.php?id=1278528366&async=1";
+cnzz_s_tag.src = "//w.cnzz.com/c.php?id=1278528366&async=1";
 var root_s = document.getElementsByTagName('script')[0];
 root_s.parentNode.insertBefore(cnzz_s_tag, root_s);
+
+
